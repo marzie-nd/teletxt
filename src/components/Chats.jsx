b75fbaa2df8743e4cb41b633e8ec304574599161
marzie-nd <marzienorouzi414@gmail.com>
2024-01-07 22:41:00 +0330

    added chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,33 @@
 const Chats = () => {
   return (
-    <div className="chats">
-      
+    <div className='chats'>
+      <div className="userChat">
+        <div className="userChatInfo">
+          <div className="user">Ahmad</div>
+          <p>ok, see you</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <div className="userChatInfo">
+          <div className="user">Milad</div>
+          <p>I'll call you later</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <div className="userChatInfo">
+          <div className="user">maman</div>
+          <p>i'll be there in a minute</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <div className="userChatInfo">
+          <div className="user">Fateme</div>
+          <p>i:DD</p>
+        </div>
+      </div>
     </div>
   )
 }
