1b9f43d5c584f8dfbeee328b25709ad0b9496439
marzie-nd <marzienorouzi414@gmail.com>
2024-01-31 17:33:56 +0330

    WIP: adduser button

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,29 +1,82 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Img from "../img/add.png";
+import "./AddUser.scss";
 
 const AddUser = ({ onAddUser }) => {
+  const [newUser, setNewUser] = useState("");
+  const [isAddingUser, setIsAddingUser] = useState(false);
 
-const [newUser, setNewUser] = useState('');
+  const handleAddButtonClick = () => {
+    setIsAddingUser(true);
+  };
 
-const handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     setNewUser(e.target.value);
-}
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (newUser.trim() !== '') {
-        onAddUser(newUser);
-        setNewUser('');
+    if (newUser.trim() !== "") {
+      onAddUser(newUser);
+      setNewUser("");
+      setIsAddingUser(false);
     }
-}
+  };
+
+  const handleBlur = () => {
+    setIsAddingUser(false);
+  };
 
   return (
-    <div className='addUser'>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={newUser} onChange={handleInputChange} placeholder='Add new user' />
-        <button type='submit'>Add</button>
-      </form>
+    <div className="addUser">
+      <div className="container">
+        {isAddingUser ? (
+          <form
+            onSubmit={handleSubmit}
+            style={{ position: "relative", display: "flex" }}
+          >
+            <input
+              type="text"
+              placeholder="Enter new username"
+              value={newUser}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              autoFocus
+            />
+            <button
+              type="submit"
+              style={{
+                backgroundColor: "transparent",
+                padding: "0",
+                margin: "0",
+              }}
+            >
+              <img
+                src={Img}
+                alt="add new user"
+                style={{ width: "60px", position: "fixed" }}
+              />
+            </button>
+          </form>
+        ) : (
+          <button
+            onClick={handleAddButtonClick}
+            style={{
+              backgroundColor: "transparent",
+              padding: "0",
+              margin: "0",
+            }}
+          >
+            <img
+              src={Img}
+              alt="add new user"
+              style={{ width: "60px", position: "fixed" }}
+            />
+          </button>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
