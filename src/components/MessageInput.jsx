4b0540550172763028b2955f53fcddbc8109aa4d
marzie-nd <marzienorouzi414@gmail.com>
2024-01-31 15:11:10 +0330

    Fixes #3: stick input to the buttom

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -12,35 +12,37 @@ const MessageInput = ({ onSendMessage }) => {
     }
   };
   return (
-    <div className="messageInput" style={{ borderButtom: "1px solid red" }}>
-      <input
-        type="text"
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-        onKeyDown={handleKeyPress}
-        placeholder="Type a message..."
-        style={{
-          borderBottom: "none"
-        }}
-      />
-      <button
-        onClick={() => {
-          onSendMessage(newMessage);
-          setNewMessage("");
-        }}
-        style={{
-          backgroundColor: "transparent",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <img
-          src={Img}
-          alt="send"
-          style={{ width: "20px", position: "absolute" }}
+    <div className="wrapper">
+      <div className="messageInput" style={{ borderButtom: "1px solid red" }}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder="Type a message..."
+          style={{
+            borderBottom: "none",
+          }}
         />
-      </button>
+        <button
+          onClick={() => {
+            onSendMessage(newMessage);
+            setNewMessage("");
+          }}
+          style={{
+            backgroundColor: "transparent",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={Img}
+            alt="send"
+            style={{ width: "20px", position: "absolute" }}
+          />
+        </button>
+      </div>
     </div>
   );
 };
