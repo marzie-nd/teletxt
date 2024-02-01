f531c76e8b77f9d6d4f76e84a0538435da82c05e
Ahmad Siavashi <siavashi@aut.ac.ir>
2024-02-01 15:07:21 +0330

    Add forgotPassword

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import Container from '../components/Container';
+import './ForgotPassword.scss';
+
+const ForgotPassword = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // TODO: complete the function
+    };
+
+    return (
+        <div className="forgotPassword">
+            <Container title="Reset Password">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <Button type="submit">Send Reset Link</Button>
+                    <Link to="/signin" className="link">Back to sign in</Link>
+                </form>
+            </Container>
+        </div>
+    );
+};
+
+export default ForgotPassword;
